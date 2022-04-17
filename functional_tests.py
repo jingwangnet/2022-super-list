22e9c3f6b7cfa19b6827428c85afbce8e79ce0f3
jing Wang <admin@jingwangnet.com>
2022-04-17 18:26:09 +0800

    Extending FTs using the unittest module.

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        if os.environ.get('HEADLESS', False):
+            options = Options()
+            options.headless = True
+            self.browser = webdriver.Chrome(options=options)
+        else:
+            self.browser = webdriver.Chrome()
 
-assert 'successful' in browser.title, browser.title
+    def tearDown(self):
+        self.browser.quit()
 
 
+    def test_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('To-Do lists', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
