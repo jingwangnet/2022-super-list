1ead234d71a3c3bb05a3488a2e1eb777d6a6a1ca
jing Wang <admin@jingwangnet.com>
2022-04-17 20:23:43 +0800

    Small design when necessary Implementing the new design incrementally using TDD Ensure have a regression test Iterating towards new design.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -68,5 +68,34 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_to_check_text_in_table('2: Eatting bread for dinner')
         self.wait_to_check_text_in_table('1: Buy bread')
 
+    def test_start_multiple_lists_on_diffrent_urls(self):
+        self.browser.get(self.live_server_url)
 
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy bread')
+        inputbox.send_keys(Keys.ENTER)
 
+        self.wait_to_check_text_in_table("1: Buy bread")
+        
+        EDITH_URL = self.browser.current_url
+        self.assertRegex(EDITH_URL, '/lists/.+/')
+
+        self.browser.quit()
+        self.setUp()
+
+        self.browser.get(self.live_server_url)
+        html = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy bread', html)
+        
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Go shopping')
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_to_check_text_in_table("1: Go shopping")
+        html = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy bread', html)
+
+        JOHN_URL = self.browser.current_url
+        self.assertRegex(JOHN_URL, '/lists/.+/')
+        self.assertNotEqual(JOHN_URL, EDITH_URL)
+        
