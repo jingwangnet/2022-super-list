0488966540ccc759b035c6d4796f721e7a876237
jing.wang <admin@jingwangnet.com>
2022-04-27 12:45:14 +0800

    Add new user story: Test layout and styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -99,3 +99,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertRegex(JOHN_URL, '/lists/.+/')
         self.assertNotEqual(JOHN_URL, EDITH_URL)
         
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=30
+        )
+
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_to_check_text_in_table('1: Buy milk')
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=30
+        )
