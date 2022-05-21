5e238e48e7be39f66828a65bf8425d75dae82484
jing.wang <admin@jingwangnet.com>
2022-05-21 20:24:32 +0800

    Add logout.

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from accounts.views import send_login_email, login
 
 app_name = 'accounts'
 urlpatterns = [
     path('send_login_email', send_login_email, name='send'),
     path('login', login, name='login'),
+    path('logout', LogoutView.as_view(next_page='/'), name='logout')
 ]
