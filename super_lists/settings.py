688e665749ee16a840b48fb8ab42f60228ab0c6a
jing.wang <admin@jingwangnet.com>
2022-05-21 19:21:47 +0800

    Custom passwordless auth backend + custom user model.

--- super_lists/settings.py
+++ super_lists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,12 @@ AUTH_USER_MODEL = 'accounts.User'
 AUTHENTICATION_BACKENDS = [
     'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
+
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = 'no_reply@jingwang.me'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+
