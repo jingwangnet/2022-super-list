6855123475aeb0a5679ecd89faff40812d4e5072
jing.wang <admin@jingwangnet.com>
2022-05-21 12:32:29 +0800

    Add url mapping for send_login_email.

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from lists import views
+from django.urls import path
+from accounts.views import send_login_email
 
+app_name = 'accounts'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name='home'),
-    path('lists/', include('lists.urls', namespace='lists')),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('send_login_email', send_login_email, name='send'),
 ]
