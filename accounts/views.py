8771f8c904571cd602095b283313a05248493740
jing.wang <admin@jingwangnet.com>
2022-05-21 13:17:50 +0800

    Test sends email to address from post.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 # Create your views here.
 def send_login_email(request):
+    email = request.POST['email']
+    send_mail('Your login link for Superlists', 'body', 'noreply@superlists', [email])
     return redirect('/')
 
