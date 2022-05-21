d15742dafa945916905a73b73203fd2e4b6b2f54
jing.wang <admin@jingwangnet.com>
2022-05-21 13:52:16 +0800

    add content into mail.body

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,12 @@ from django.core.mail import send_mail
 # Create your views here.
 def send_login_email(request):
     email = request.POST['email']
-    send_mail('Your login link for Superlists', 'body', 'noreply@superlists', [email])
+    send_mail(
+        'Your login link for Superlists', 
+        'Use this link to log in', 
+        'noreply@superlists', 
+        [email]
+    )
 
     messages.success(
         request, 
